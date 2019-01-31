5b36af2e220f799e4fbfb801738744b5a78b1e22
ludabeiz <david.15_@hotmail.com>
2019-01-31 14:41:39 -0500

    registro

--- examen-web/src/app.controller.ts
+++ examen-web/src/app.controller.ts
@@ -4,36 +4,36 @@ import {UsuarioService} from "./usuario/usuario.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly _appService: AppService,
-              private readonly _usuarioService: UsuarioService) {}
+    constructor(private readonly _appService: AppService,
+                private readonly _usuarioService: UsuarioService) {}
 
-  @Get('login')
- mostrarLogin(
-     @Res() res
-  ){
-    res.render('login')
-  }
+    @Get('login')
+    mostrarLogin(
+        @Res() res
+    ){
+        res.render('login')
+    }
 
 
-  @Post('login')
+    @Post('login')
     metodoLogin(
         @Body('username') username:string,
         @Body('password') password: string,
         @Res () res,
         @Session() sesion, ){
 
-      const respuesta = await this._usuarioService.autenticar(username, password)
-      if (respuesta){
-          sesion.usuario= username;
-          res.send('ok')
-      }else
+        const respuesta = await this._usuarioService.autenticar(username, password)
+        if (respuesta){
+            sesion.usuario= username;
+            res.send('ok')
+        }else
 
 
 
-  }
+            }
 
 
 
 
-  ){}
+){}
 }
