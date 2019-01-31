a79ad15b347bd335d236dad6d8bcfd5edb2e4c2c
Miguel-Parra <miguelts644@hotmail.com>
2019-01-31 14:42:37 -0500

    hola

--- examen-web/src/app.controller.ts
+++ examen-web/src/app.controller.ts
@@ -4,36 +4,45 @@ import {UsuarioService} from "./usuario/usuario.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly _appService: AppService,
-              private readonly _usuarioService: UsuarioService) {}
+    constructor(private readonly _appService: AppService,
+                private readonly _usuarioService: UsuarioService) {
+    }
 
-  @Get('login')
- mostrarLogin(
-     @Res() res
-  ){
-    res.render('login')
-  }
+    @Get('login')
+    mostrarLogin(
+        @Res() res
+    ) {
+        res.render('login')
+    }
 
 
-  @Post('login')
-    metodoLogin(
-        @Body('username') username:string,
+    @Post('login')
+   async  metodoLogin(
+        @Body('username') username: string,
         @Body('password') password: string,
-        @Res () res,
-        @Session() sesion, ){
+        @Res() res,
+        @Session() sesion,) {
 
-      const respuesta = await this._usuarioService.autenticar(username, password)
-      if (respuesta){
-          sesion.usuario= username;
-          res.send('ok')
-      }else
+        const respuesta = await this._usuarioService.autenticar(username, password)
+        const idUsuario = respuesta.id;
+        const nombre = respuesta.nombre_usuario;
+        const ses = {
+            roles: [{
+                id: ,
+                nombre: ''
+            }]
+        }
+no
+          if (respuesta) {
+            sesion.usuario = username;
+            res.send('ok')
+        } else {
+            res.redirect('login')
+        }
 
-
-
-  }
-
-
-
-
-  ){}
+    }
 }
+
+
+
+
